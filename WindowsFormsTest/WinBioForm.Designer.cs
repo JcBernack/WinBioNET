34849ae6baeb958a66965dfc690e2e791a1e90ff
JcBernack <jc.bernack@gmail.com>
2015-07-23 15:33:58 +0200

    Add test application using Windows Forms

---
+++ WindowsFormsTest/WinBioForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsTest
+{
+    partial class WinBioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonIdentify = new System.Windows.Forms.Button();
+            this.buttonLocateSensor = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonIdentify
+            // 
+            this.buttonIdentify.Location = new System.Drawing.Point(122, 12);
+            this.buttonIdentify.Name = "buttonIdentify";
+            this.buttonIdentify.Size = new System.Drawing.Size(104, 23);
+            this.buttonIdentify.TabIndex = 0;
+            this.buttonIdentify.Text = "Identify";
+            this.buttonIdentify.UseVisualStyleBackColor = true;
+            this.buttonIdentify.Click += new System.EventHandler(this.buttonIdentify_Click);
+            // 
+            // buttonLocateSensor
+            // 
+            this.buttonLocateSensor.Location = new System.Drawing.Point(12, 12);
+            this.buttonLocateSensor.Name = "buttonLocateSensor";
+            this.buttonLocateSensor.Size = new System.Drawing.Size(104, 23);
+            this.buttonLocateSensor.TabIndex = 1;
+            this.buttonLocateSensor.Text = "LocateSensor";
+            this.buttonLocateSensor.UseVisualStyleBackColor = true;
+            this.buttonLocateSensor.Click += new System.EventHandler(this.buttonLocateSensor_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(576, 283);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(232, 12);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // WinBioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 336);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.buttonLocateSensor);
+            this.Controls.Add(this.buttonIdentify);
+            this.Name = "WinBioForm";
+            this.Text = "WinBioForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIdentify;
+        private System.Windows.Forms.Button buttonLocateSensor;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ WindowsFormsTest/WinBioForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using WinBioNET;
+using WinBioNET.Enums;
+
+namespace WindowsFormsTest
+{
+    public partial class WinBioForm
+        : Form
+    {
+        private static readonly Guid DatabaseId = Guid.Parse("BC7263C3-A7CE-49F3-8EBF-D47D74863CC6");
+        private WinBioSessionHandle _session;
+        private int _unitId;
+
+        public WinBioForm()
+        {
+            InitializeComponent();
+        }
+
+        protected void Log(string message)
+        {
+            if (richTextBox.InvokeRequired)
+            {
+                richTextBox.Invoke(new Action<string>(Log), message);
+                return;
+            }
+            richTextBox.AppendText(message + "\n");
+        }
+
+        protected void Log(WinBioException exception)
+        {
+            Log(exception.Message);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var units = WinBio.EnumBiometricUnits(WinBioBiometricType.Fingerprint);
+            Log(string.Format("Found {0} units", units.Length));
+            if (units.Length == 0) return;
+            var unit = units[0];
+            _unitId = unit.UnitId;
+            Log(string.Format("Using unit id: {0}", _unitId));
+            Log(string.Format("Device instance id: {0}", unit.DeviceInstanceId));
+            Log(string.Format("Using database: {0}", DatabaseId));
+            _session = WinBio.OpenSession(WinBioBiometricType.Fingerprint, WinBioPoolType.Private, WinBioSessionFlag.Basic, new[] { _unitId }, DatabaseId);
+            Log("Session opened: " + _session.Value);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            WinBio.CloseSession(_session);
+            _session.Invalidate();
+            Log("Session closed");
+        }
+
+        private void buttonLocateSensor_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                Log("Locating sensor...");
+                try
+                {
+                    var unitId = WinBio.LocateSensor(_session);
+                    Log(string.Format("Sensor located: unit id {0}", unitId));
+                }
+                catch (WinBioException ex)
+                {
+                    Log(ex);
+                }
+            });
+        }
+
+        private void buttonIdentify_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                Log("Identifying user...");
+                try
+                {
+                    WinBioIdentity identity;
+                    WinBioBiometricSubType subFactor;
+                    WinBioRejectDetail rejectDetail;
+                    WinBio.Identify(_session, out identity, out subFactor, out rejectDetail);
+                    Log(string.Format("Identity: {0} ({1})", identity, subFactor));
+                }
+                catch (WinBioException ex)
+                {
+                    Log(ex);
+                }
+            });
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            WinBio.Cancel(_session);
+        }
+    }
+}
